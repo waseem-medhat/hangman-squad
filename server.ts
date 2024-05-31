72cb77cc47eaebdf58623728d0e1774d72d0b3f7
waseem-medhat <wipdev.tech@gmail.com>
2024-05-31 20:03:23 +0300

    Set up basic web server

--- server.ts
+++ server.ts
@@ -1,2 +1,12 @@
-const message: string = "Hi"
-console.log(message)
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+const port = process.env.PORT || 8080
+
+const app = express()
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.get("/v1", (_req, res) => res.json({ message: "Welcome to Hangman Squad API V1!" }))
+app.listen(port, () => console.log(`Hangman Squad server listening to :${port}`))
